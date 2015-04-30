fa63ba7a206f6b3f932d6c1621589a1aa672889f
muelljef <muelljef@onid.oregonstate.edu>
2015-04-30 14:17:12 -0700

    got the login, session counter, logout process working, need to implement session encapsulation on content1 and content2 yet

--- content1.php
+++ content1.php
@@ -1,50 +1,61 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Content 1</title>
-    </head>
-<body>
-
-<?php
 session_start();
 //session related variable and function calls
 //reference from OSU's lectures and lecture code
+
+echo '<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Content 1</title>
+</head>
+<body>';
+
 function redirect(){
-    session_destroy();
     $filePath = explode('/', $_SERVER['PHP_SELF'], -1);
     $filePath = implode('/',$filePath);
-    $redirect = "http://" . $_SERVER['HTTP_HOST'] . $filePath . "/login.php";
-    //header("Location: {$redirect}/login.php", true);
+    $redirect = "http://" . $_SERVER['HTTP_HOST'] . $filePath;
     return $redirect;
 }
 
 function loginLink() {
-    echo '<a href=' . redirect() . '>Here</a>';
+    $loginString = redirect() . "/login.php";
+    echo '<p>A username must be entered. Click ';
+    echo "<a href=$loginString>here</a>";
+    echo ' to return to the login screen';
+}
+
+function displayContent() {
+    $logoutString = redirect() . "/logout.php";
+    echo '<p>' . "Hi $_SESSION[username], you have visited this page $_SESSION[visits] times. Click ";
+    echo "<a href=$logoutString>here</a> to logout";
+    echo '<p><a href="content2.php">Content 2</a><br>';
+    $_SESSION['visits']++;
 }
 
 if (session_status() == PHP_SESSION_ACTIVE) {
     if (isset($_POST['username'])) {
-        if ($_POST['username'] == '' || is_null(['username'])){
-            //add redirect function call here
-            echo "you have not set the username";
+        //if the POST username is set
+        if ($_POST['username'] == '' || is_null(['username'])) {
+            //if POST user name is null or empty string show return link
             loginLink();
         } else {
+            //otherwise it is not an empty string, set the username
             $_SESSION['username'] = $_POST['username'];
             if(!isset($_SESSION['visits'])){
+                //if # of visits doesn't exist initialize it
                 $_SESSION['visits'] = 0;
             }
-
-            $_SESSION['visits']++;
-            echo "Hi $_SESSION[username], you have visited this page $_SESSION[visits] times. \n";
+            //increment the visits
+            displayContent();
         }
+    } elseif (isset($_SESSION['username'])){
+        //if the post has not been set, and the session username is set
+        displayContent();
     }
 }
-?>
 
-</body></html>
+echo '</body></html>';
+?>
---
+++ content2.php
@@ -0,0 +1,12 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+echo '<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Content 2</title>
+</head>
+<body>Hello<br>
+<a href="content1.php">Content 1</a>
+</body></html>';
